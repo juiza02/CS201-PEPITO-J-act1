f7fafc4830332ecdf2919f05ff381b41e1e650fd
juiza02 <145626082+juiza02@users.noreply.github.com>
2023-10-20 13:56:41 +0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,27 @@
+var firstFavNum=14;
+console.log('First favorite num:' + firstFavNum);
+
+var secondFavNum=2;
+console.log('Second favorite num:' + secondFavNum);
+
+
+//addition
+var sum = firstFavNum + secondFavNum;
+console.log('firstFavNum + secondFavNum' + sum);
+
+// subtract
+var sum = firstFavNum - secondFavNum;
+console.log('firstFavNum - secondFavNum=' + sum);
+
+//multiply
+var sum = firstFavNum * secondFavNum;
+console.log('firstFavNum * secondFavNum=' + sum);
+
+// divide
+var sum = firstFavNum / secondFavNum;
+console.log('firstFavNum / secondFavNum=' + sum);
+
+console.log(firstFavNum % secondFavNum);
+
+let pow=Math.pow(firstFavNum,secondFavNum);
+console.log(pow);
